845e1837fee0cbf63e692f2ae6edf243fb300ee7
bharatdevparmar <bharatdevparmar@gmail.com>
2020-03-07 16:56:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from "./Components/signIn";
+function App() {
+  return (
+   <SignUp/>
+  );
+}
+
+export default App;

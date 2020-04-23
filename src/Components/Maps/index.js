6617865e195991315a9e879d930d378ff652e9f6
poojasahani254 <poojasahani254@gmail.com>
2020-04-23 17:59:24 +0530

    Adding location

--- src/Components/Maps/index.js
+++ src/Components/Maps/index.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import { geolocated } from "react-geolocated";
 
 
- function ScrollableTabsButtonAuto(props) {
+ const Maps = (props) => {
   //  console.log('loc',props)
-     return (
-        <Map google={props.google} zoom={14}  initialCenter={{
-          lat: 21.193820,
-          lng: 72.786850
-        }}>
-        <Marker name={'Current location'} />
-        <InfoWindow>
-        </InfoWindow>
-      </Map>
-    );
+     const [Position,setPosition]=useState({
+         latitude:'',
+         longitude:''
+     })
+     useEffect(()=>{
+         navigator.geolocation.getCurrentPosition(function(position) {
+             console.log("Latitude is :", position.coords.latitude);
+             console.log("Longitude is :", position.coords.longitude);
+             setPosition({
+                 latitude: position.coords.latitude,
+                 longitude: position.coords.longitude
+             })
+         },function () {
+
+         },{enableHighAccuracy: true,timeout: 20000,
+             maximumAge: 1000});
+     },[])
+     const onMarkerClick = () =>{
+
+         alert('hello')
+     }
+    const onMarkerDragEnd = (e) =>{
+         alert('dfd')
+
+    }
+
+     if(Position.longitude && Position.latitude){
+         return (
+             <Map google={props.google} zoom={14}
+                  initialCenter={{
+                 lat: Position.latitude,
+                 lng: Position.longitude
+             }}>
+                 <Marker name={'Current location'}
+                         draggable={true}
+                         onClick={onMarkerClick}
+                         onDragEnd={onMarkerDragEnd} />
+                 {/*<InfoWindow>*/}
+                 {/*    <div>*/}
+                 {/*        <h1>{this.state.selectedPlace.name}</h1>*/}
+                 {/*    </div>*/}
+                 {/*</InfoWindow>*/}
+             </Map>
+         );
+     }else{
+         return <div>Error</div>
+     }
+
+
 }
 
 export default GoogleApiWrapper({
     apiKey: ("AIzaSyB9HQsSCOPZF2WZSO3Vp4s_Q8BUAN79o-o")
-  })(geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-})(ScrollableTabsButtonAuto))
+  })(Maps)
